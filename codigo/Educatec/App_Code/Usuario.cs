cdc6218f0f3d4ae2c39d5e3e4a102218dfa037da
mateus alves <mentrixmax@gmail.com>
2018-06-19 21:03:56 -0300

    teste

---
+++ codigo/Educatec/App_Code/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Menfram.Entity.Interface;
+using Menfram.DAL.Reflection;
+
+/// <summary>
+/// Summary description for Usuario
+/// </summary>
+public class Usuario : IEntity
+{
+	public Usuario()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    [NoField]
+    public string DescriptionComboBox
+    {
+        get { return ""; }
+    }
+
+   [NoField]
+    public string Key
+    {
+        get { return ""; }
+    }
+
+    public int? Sequencial
+    {
+        get;
+        set;
+    }
+}
