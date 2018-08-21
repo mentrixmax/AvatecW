99c633608c3a1d3f77ead5e02e42b99ceeb74714
User <User@USER-PC>
2018-08-20 21:32:44 -0300

    Gravar codigo feito pelo marcos, da InstituicaoOrigem

---
+++ codigo/Educatec/App_Code/InstituicaoOrigem.cs
@@ -0,0 +1,41 @@
+﻿using Menfram.DAL.Reflection;
+using Menfram.Entity.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for InstituicaoOrigem
+/// </summary>
+[Table(TableName = "instituicaoOrigem")]
+public class InstituicaoOrigem : IEntity
+{
+    public InstituicaoOrigem()
+    {
+
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    [NoField]
+    public string DescriptionComboBox
+    {
+        get { return ""; }
+    }
+
+    [NoField]
+    public string Key
+    {
+        get { return ""; }
+    }
+
+    [Key(Pk = true, KKey = true)]
+    public int? Sequencial
+    {
+        get;
+        set;
+    }
+
+    public String DesInstituicaoOrigem { get; set; }
+}
---
+++ codigo/Educatec/App_Code/InstituicaoOrigemDAO.cs
@@ -0,0 +1,34 @@
+﻿using Menfram.DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for InstituicaoOrigemDAO
+/// </summary>
+public class InstituicaoOrigemDAO : AbstractDAO<InstituicaoOrigem>
+{
+    public InstituicaoOrigemDAO()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    private static InstituicaoOrigemDAO dao = null;
+    public static InstituicaoOrigemDAO getInstance()
+    {
+        if (dao == null)
+        {
+            dao = new InstituicaoOrigemDAO();
+
+        }
+        return dao;
+
+    }
+
+    public override Type getTipo()
+    {
+        return typeof(InstituicaoOrigem);
+    }
+}
