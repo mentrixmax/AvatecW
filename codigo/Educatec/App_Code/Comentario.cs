f086ec2e545d4400a1c8df2315c58333e4a85611
User <User@USER-PC>
2018-08-21 20:51:14 -0300

    Correção feia pelo marcos

---
+++ codigo/Educatec/App_Code/Comentario.cs
@@ -0,0 +1,45 @@
+﻿using Menfram.DAL.Reflection;
+using Menfram.Entity.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Comentário
+/// </summary>
+[Table("Comentário")]
+public class Comentario : IEntity
+
+{
+    public Comentario()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    [NoField]
+    public string DescriptionComboBox
+    {
+        get { return ""; }
+    }
+
+    [NoField]
+    public string Key
+    {
+        get { return ""; }
+    }
+
+    [Key(Pk = true, KKey = true)]
+    public int? Sequencial
+    {
+        get;
+        set;
+    }
+
+    public String DesComentario { get; set; }
+    public int? PostID { get; set; }
+    public DateTime? Data { get; set; }
+
+}
---
+++ codigo/Educatec/App_Code/ComentarioDAO.cs
@@ -0,0 +1,34 @@
+﻿using Menfram.DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ComentárioDAO
+/// </summary>
+public class ComentarioDAO : AbstractDAO<Comentario>
+{
+    public ComentarioDAO()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    private static ComentarioDAO dao = null;
+    public static ComentarioDAO getInstance()
+    {
+        if (dao == null)
+        {
+            dao = new ComentarioDAO();
+
+        }
+        return dao;
+
+    }
+
+    public override Type getTipo()
+    {
+        return typeof(Comentario);
+    }
+}
