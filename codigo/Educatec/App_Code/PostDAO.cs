6fbefee4a0419e63d43f87d801504cbdb68b2bdf
mentrixmax <mentrixmax@gmail.com>
2018-08-21 21:07:12 -0300

    Class criada por Thalya

---
+++ codigo/Educatec/App_Code/PostDAO.cs
@@ -0,0 +1,35 @@
+﻿using Menfram.DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PostDAO
+/// </summary>
+public class PostDAO : AbstractDAO<PostEntity>
+{
+
+    public PostDAO()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    private static PostDAO dao = null;
+    public static PostDAO getInstance()
+    {
+        if (dao == null)
+        {
+            dao = new PostDAO();
+
+        }
+        return dao;
+
+    }
+
+    public override Type getTipo()
+    {
+        return typeof(PostEntity);
+    }
+}
---
+++ codigo/Educatec/App_Code/PostEntity.cs
@@ -0,0 +1,43 @@
+﻿using Menfram.DAL.Reflection;
+using Menfram.Entity.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PostEntity
+/// </summary>
+[Table("Postagem")]
+public class PostEntity : IEntity
+{
+    public PostEntity()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    [NoField]
+    public string DescriptionComboBox
+    {
+        get { return ""; }
+    }
+
+    [NoField]
+    public string Key
+    {
+        get { return ""; }
+    }
+
+    [Key(Pk = true, KKey = true)]
+    public int? Sequencial
+    {
+        get;
+        set;
+    }
+
+    public String DesPostEntity { get; set; }
+    public int? PostID { get; set; }
+    public DateTime? Data { get; set; }
+
+}
